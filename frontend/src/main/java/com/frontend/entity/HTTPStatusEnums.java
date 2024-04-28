94779b0763fe60be2ca19ef7e4c4e3d4ce459f40
charlie_can <2016133456@qq.com>
2024-04-28 10:37:14 +0800

    完成前端基础界面切换 完成用户登录GetUserINfo

---
+++ frontend/src/main/java/com/frontend/entity/HTTPStatusEnums.java
@@ -0,0 +1,22 @@
+package com.frontend.entity;
+
+public enum HTTPStatusEnums {
+
+    OK(200),
+    Not_Connected_Server(419),
+    Unknown_Error(600);
+
+
+    private final Integer code;
+
+
+    HTTPStatusEnums(Integer code) {
+        this.code = code;
+
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+}
